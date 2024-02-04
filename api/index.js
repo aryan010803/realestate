f000e8bcea779fbe4a82baa2c1a2c659d9844c44
aryan010803 <102131111+aryan010803@users.noreply.github.com>
2024-02-05 01:02:28 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import UserRouter from './routes/user.route.js'
+import authRouter  from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected");
@@ -9,7 +10,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 const app = express();
+app.use(express.json());
 app.listen(3000 ,()=>{
     console.log("running");
 })
-app.use('/api/user' , UserRouter)
+app.use('/api/user' , UserRouter);
+app.use('/api/auth' , authRouter);
---
+++ api/routes/auth.route.js
@@ -0,0 +1,5 @@
+import express from 'express'
+import { signup } from '../controller/auth.controller.js';
+const router  = express();
+router.post('/signup', signup)
+export default router;
