4824c0b0692c063ec8b975840b9f7c3f8f91c784
aryan010803 <102131111+aryan010803@users.noreply.github.com>
2024-02-04 14:02:50 +0530

    create header componenet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signout from './pages/Signout'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element ={<About/>}/>
