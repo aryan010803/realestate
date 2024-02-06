b4ed29e5f478763f9cafc2efff0e3bbfbb935aad
aryan010803 <102131111+aryan010803@users.noreply.github.com>
2024-02-06 16:48:05 +0530

    Created signup page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
-import Signout from './pages/Signout'
+import Signup from './pages/Signup'
 import Header from './Components/Header'
 
 const App = () => {
@@ -15,8 +15,8 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element ={<About/>}/>
         <Route path='/profile' element  = {<Profile/>}/>
-        <Route path='signin' element = {<Signin/>}/>
-        <Route path='signout' element ={<Signout/>}/>
+        <Route path='/signin' element = {<Signin/>}/>
+        <Route path='/signup' element ={<Signup/>}/>
 
       </Routes>
     </BrowserRouter>
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+   <div>signin</div>
   )
 }
 
