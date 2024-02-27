be9d722c4eb1182addc53d87bc53e2606659f054
aryan010803 <102131111+aryan010803@users.noreply.github.com>
2024-02-27 21:17:29 +0530

    update for deployment

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ import UserRouter from './routes/user.route.js'
 import authRouter  from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from '../api/routes/listing.route.js'
+import path from 'path'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected");
 }).catch((err)=>{
     console.log(err);
 })
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -21,6 +23,10 @@ app.listen(3000 ,()=>{
 app.use('/api/user' , UserRouter);
 app.use('/api/auth' , authRouter);
 app.use('/api/listing',listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((err , req , res , next)=>{
     const statusCode = err.statusCode ||500;
     const message = err.message || "Internal Server error";
