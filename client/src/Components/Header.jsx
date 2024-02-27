d4d377d93a1984754c9061ad5bb0dd991943c742
aryan010803 <102131111+aryan010803@users.noreply.github.com>
2024-02-27 20:53:17 +0530

    update about page

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -28,8 +28,8 @@
       <Link to='/'>
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
 
-        <span className='text-slate-500'>Real</span>
-        <span className='text-slate-700'>Estate</span>
+        <span className='text-slate-500'>Home</span>
+        <span className='text-slate-700'>Harbor</span>
       </h1>
       </Link>
         <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg  flex items-center'>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='flex flex-col max-w-[720px] gap-4 my-7 items-center mx-auto'>
+      <h1 className='text-slate-500 font-bold text-4xl mb-9'>Home<span className='text-slate-700'>Harbor</span></h1>
+      <p className='text-slate-700 font-semibold'>
+        Introducing Home Harbor, your premier
+        destination for navigating the seas of real estate in search
+        of the perfect abode. Our website is meticulously crafted to provide a user-friendly
+        experience, offering a curated collection of properties that span diverse architectural styles and cater
+        to varying lifestyle preferences. Home Harbor sets sail as a beacon of reliability and innovation, presenting an
+        intuitive platform that guides users through a myriad of listings effortlessly. Whether you're drawn to the allure of
+        city apartments or the tranquility of suburban
+        estates, Home Harbor is your compass on the journey to discovering the home that speaks to your unique sense of comfort and style.
+      </p>
+
+      <p className='text-slate-700 font-semibold'>
+        At Home Harbor, we recognize that a home is more than just a structure;
+        it's a sanctuary where dreams unfold and memories are made. Going beyond
+        conventional listings, our platform offers insightful guides, market trends,
+        and neighborhood highlights to empower users with informed decision-making.
+        Whether you're a seasoned investor, a first-time buyer, or someone embarking
+        on a fresh chapter, Home Harbor invites you to explore the vast horizons of real estate.
+        With a fusion of innovation and personalized service, we navigate the waves of the housing market to help you
+        find a harbor that truly feels like home. Home Harbor - Guiding You to Your Perfect Port.
+
+
+
+
+
+
+      </p>
+    </div>
   )
 }
 
